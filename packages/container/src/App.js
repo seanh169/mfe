314dee361353c197f6dce630e73fd524072e8c50
Sean Hamilton <seanh169@gmail.com>
2022-08-30 08:52:41 -0400

    adding namespace for css produciton files

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,16 +2,21 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
-// console.log(mount)
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co',
+})
 
 export default () => {
     return (
-        <BrowserRouter>
-        <div>
-            <Header />
-            <MarketingApp />
-        </div>
-        </BrowserRouter>
+        <StylesProvider generateClassName={generateClassName}>
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <MarketingApp />
+                </div>
+            </BrowserRouter>
+        </StylesProvider>
     )
 }
